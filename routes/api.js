89a98723e30e5833c158bd3e805407615ddd3af7
mahdi ghazaei <mahdighazaei8@gmail.com>
2022-10-27 16:43:57 +0000

    add /api/books get and post method handlers

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,56 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+const Book = require("../models/book");
+const Comment = require("../models/comment");
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(async function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      const allBooks = await Book.find({});
+      res.json(allBooks);
     })
-    
-    .post(function (req, res){
+
+    .post(async function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (title == "" || title == undefined)
+        return res.send("missing required field title");
+      const newBook = await Book.create({ title });
+      const bookObj = newBook.toObject();
+      delete bookObj.__v;
+      delete bookObj.commentcount;
+      res.json(bookObj);
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
